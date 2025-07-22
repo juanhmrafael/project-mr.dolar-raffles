a4f770e052fb733d85bdcd0609635b0c00a24129
Juan Rafael <juanhmrafael@gmail.com>
2025-07-22 14:04:24 -0400

    Actualización

--- frontend/src/app/features/location-page/location-page.ts
+++ frontend/src/app/features/location-page/location-page.ts
@@ -14,7 +14,7 @@ export class LocationPage {
         name: 'Mr. Dólar',
         address:
             'Entre Manrique y Cantaura, Calle Boyaca, Casa 94 21 Toldo Amarillo, Valencia 2001, Carabobo',
-        phone: '(0424) 419-9668',
+        phone: '(0424) 474-3384',
         googleMapsUrl: 'https://maps.app.goo.gl/9f66rh1vfoun2N8i9',
         googleMapsEmbedUrl:
             'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3927.0410650214217!2d-68.0012058!3d10.1773176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e806722b77484e5%3A0x84750ebc8bf546a5!2smisterdolar1!5e0!3m2!1ses!2sve!4v1752878654857!5m2!1ses!2sve',
